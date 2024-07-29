c6008c9d5d63b1c724ed6d6e6fea1feadd8a1026
tushcmd <muturidavid854@gmail.com>
2024-07-29 13:58:10 +0300

    feat: protect /generate page if not authenticated

--- app/(protected)/generate/page.tsx
+++ app/(protected)/generate/page.tsx
@@ -1,12 +1,12 @@
 import Flashcard from '@/components/flash-card';
 import { getCurrentUser } from '@/lib/session'
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 
 export default async function Page() {
     const user = await getCurrentUser();
 
-    // if (!user) redirect("/");
+    if (!user) redirect("/");
 
     return (
         <div>
